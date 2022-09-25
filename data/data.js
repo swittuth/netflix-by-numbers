dedbfe32d2167f92c53cec3cfe765c11f58f7489
swittuthcoding <swittuthcoding@gmail.com>
2022-09-25 16:52:48 -0400

    transformed data

--- data/data.js
+++ data/data.js
@@ -1,9 +1,23 @@
 import * as d3 from "d3";
+import moment from "moment/moment.js";
 
 const url =
   "https://raw.githubusercontent.com/swittuth/netflix-by-numbers/main/data/netflix_titles.csv";
 
-const data = await d3.csv(url);
-console.log(data);
-
-export default data;
+export const data = await d3.csv(url, (data) => ({
+  show_id: data.show_id,
+  type: data.type,
+  title: data.title,
+  director: data.director,
+  cast: data.cast,
+  country: data.country,
+  date_added: moment(data.date_added, "MMMM DD YYYY").toDate(),
+  release_year: data.release_year,
+  rating: data.rating,
+  duration:
+    data.duration.indexOf("min") >= 0
+      ? Number(data.duration.split(" ")[0])
+      : data.duration,
+  listed_in: data.listed_in,
+  description: data.description,
+}));
---
+++ src/analysis/agg-analysis.js
@@ -0,0 +1,4 @@
+import { data } from "../../data/data.js";
+import * as d3 from "d3";
+
+console.log(data);
