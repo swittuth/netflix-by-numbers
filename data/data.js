d04184b654e9abea907c63ae1a078ce576f39203
swittuthcoding <swittuthcoding@gmail.com>
2022-09-25 16:06:55 -0400

    resolved data for analysis

--- data/data.js
+++ data/data.js
@@ -2,4 +2,8 @@ import * as d3 from "d3";
 
 const url =
   "https://raw.githubusercontent.com/swittuth/netflix-by-numbers/main/data/netflix_titles.csv";
-d3.csv(url).then((data) => console.log(data));
+
+const data = await d3.csv(url);
+console.log(data);
+
+export default data;
