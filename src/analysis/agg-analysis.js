31e162f84ca551f99d685f5ff1d2ea1912e05ab3
swittuthcoding <swittuthcoding@gmail.com>
2022-09-25 18:09:42 -0400

    added analysis

--- src/analysis/agg-analysis.js
+++ src/analysis/agg-analysis.js
@@ -1,4 +1,24 @@
 import { data } from "../../data/data.js";
 import * as d3 from "d3";
 
-console.log(data);
+// latest and earliest added for movies and tv shows
+const latestTime = d3.rollup(
+  data,
+  (data) => {
+    return d3.extent(data, (d) => d.date_added);
+  },
+  (data) => data.type
+);
+
+// number of countries that the show/movie was produced in
+const countries = new Set();
+for (let d of data) {
+  const list = d.country.split(",");
+  for (let c of list) {
+    if (c.length > 0) countries.add(c.trim());
+  }
+}
+console.log(countries.size);
+
+// tv shows / movies added by the year 
+const showsByYear = 
